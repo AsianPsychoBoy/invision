2e5b511537babf846ddd27849483d8e343b15bbe
Jack Cai <ziyang_99@outlook.com>
2017-07-17 16:18:07 +0800

    加菜单

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,6 +1,13 @@
 <template>
 	<div id="app">
-		<nav class="app-menu">
+		<div class="app-menu">
+			<div class="window-button-group">
+				<button class="minimize">-</button>
+				<button class="maximize">+</button>
+				<button class="close">X</button>
+			</div>
+		</div>
+		<nav class="app-nav">
 			<router-link to="/room-list">房间列表</router-link>
 			<router-link to="/create-room">创建直播</router-link>
 			<router-link to="/profile">我的资料</router-link>
@@ -17,4 +24,5 @@
 
 <style>
   /* CSS */
+
 </style>
